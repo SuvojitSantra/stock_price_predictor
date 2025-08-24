00ae71182e9357db85550318674d3f5879ccfd59
SuvojitSantra <75536197+SuvojitSantra@users.noreply.github.com>
2025-08-25 04:22:24 +0530

    Add files via upload

--- web_stock_price_predictor.py
+++ web_stock_price_predictor.py
@@ -16,22 +16,110 @@ start = datetime(end.year-20,end.month,end.day)
 
 google_data = yf.download(stock, start, end)
 
-model = load_model("Latest_stock_price_model.keras")
+#model = load_model("Latest_stock_price_model.keras  D:\stock price predictor\Latest_stock_price_model.keras ")
+model = load_model(r"D:\stock price predictor\Latest_stock_price_model.keras")
+
 st.subheader("Stock Data")
 st.write(google_data)
 
-spliting_len = int(len(Google_data)*0.7)
+spliting_len = int(len(google_data)*0.7)
 x_test = pd.DataFrame(google_data.Close[spliting_len:])
 
-def plot_graph(figsize, values, full_data):
+def plot_graph(figsize, values, full_data, extra_data = 0, extra_dataset = None):
     fig = plt.figure(figsize=figsize)
     plt.plot(values,'Orange')
-    plt.plot(full_data.close, 'b')
+    plt.plot(full_data.Close, 'b')
+    if extra_data:
+        plt.plot(extra_dataset)
     return fig
 
 
+st.subheader('Original Close Price and MA for 250 days')
+google_data['MA_for_250_days'] = google_data.Close.rolling(250).mean()
+st.pyplot(plot_graph((15,6), google_data['MA_for_250_days'],google_data,0))
+
+st.subheader('Original Close Price and MA for 200 days')
+google_data['MA_for_200_days'] = google_data.Close.rolling(200).mean()
+st.pyplot(plot_graph((15,6), google_data['MA_for_200_days'],google_data,0))
+
+st.subheader('Original Close Price and MA for 100 days')
+google_data['MA_for_100_days'] = google_data.Close.rolling(100).mean()
+st.pyplot(plot_graph((15,6), google_data['MA_for_100_days'],google_data,0))
+
+
+st.subheader('Original Close Price and and MA for 100 days MA for 250 days')
+st.pyplot(plot_graph((15,6), google_data['MA_for_250_days'],google_data,1,google_data['MA_for_250_days']))
+
+
+from sklearn.preprocessing import MinMaxScaler
+
+# scaler = MinMaxScaler(feature_range=(0,1))
+# print("x_test shape:", x_test.shape)
+# print("x_test columns:", x_test.columns.tolist())
+# print(x_test.head())
+
+
+scaler = MinMaxScaler(feature_range=(0,1))
+print("x_test shape:", x_test.shape)
+print("x_test columns:", x_test.columns.tolist())
+print(x_test.head())
+
+scaled_data = scaler.fit_transform(x_test)
+
+
+#scaled_data = scaler.fit_transform(x_test[['Close']])
+
+
+
+x_data = []
+y_data = []
+
+for i in range (100, len(scaled_data)):
+  x_data.append(scaled_data[i-100:i])
+  y_data.append(scaled_data[i])
+
+x_data, y_data = np.array(x_data),np.array(y_data)
+
+predictions = model.predict(x_data)
+
+inv_pre = scaler.inverse_transform(predictions)
+inv_y_test = scaler.inverse_transform(y_data)
+
+ploting_data = pd.DataFrame(
+    {
+        'original_test_data': inv_y_test.reshape(-1),
+        'predictions': inv_pre.reshape(-1)
+
+    },
+    index = google_data.index[spliting_len+100:]
+)
+st.subheader("Orginal Values VS Predicted Values")
+st.write(ploting_data)
+
+
+st.subheader('Original Close Price VS Predicted Close Price')
+fig = plt.figure(figsize=(15,6))
+plt.plot(pd.concat([google_data.Close[:spliting_len+100],ploting_data], axis=0))
+plt.legend(["Data not used", "Original Test Data","Predicted Test Data"])
+st.pyplot(fig)
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# venv\Scripts\activate
+# streamlit run "d:\stock price predictor\web_stock_price_predictor.py"
 
